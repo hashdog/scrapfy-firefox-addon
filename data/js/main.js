a276be17d9b7920dd4074dc7f4e4b7ff05a1ce0e
Mario Colque <colquemario@gmail.com>
2014-07-16 18:24:03 -0300

    added loading

--- data/js/main.js
+++ data/js/main.js
@@ -1,7 +1,17 @@
 var langSelector = document.getElementById('lang-selector');
 var createButton = document.getElementById('create-btn');
+var createText = document.getElementById('create-text');
+var loadingText = document.getElementById('loading-text');
+
+self.port.on('opened', function() {
+  createText.style.display = 'block';
+  loadingText.style.display = 'none';
+});
 
 createButton.addEventListener('click', function() {
+  createText.style.display = 'none';
+  loadingText.style.display = 'block';
+
   var lang = langSelector.value;
   self.port.emit('create', lang);
 }, false);
